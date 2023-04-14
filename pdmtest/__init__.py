7e7f43056e8abce56307395ad65d1f3cee403b23
Philipp Jaschke <philipp.jaschke@schulz.st>
2023-04-14 07:55:05 +0200

    init

---
+++ pdmtest/__init__.py
@@ -0,0 +1,5 @@
+from ._core import greeter
+
+__all__ = [
+    "greeter"
+]
---
+++ pdmtest/_core.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+def greeter(name: str):
+    print(f"Moin Welt {name}")
+    print(BaseModel)
